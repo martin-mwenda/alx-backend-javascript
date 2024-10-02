6ca1a6465c036ea5f29417b95729579f4537165f
Martin Mwirigi <mwirigi.8kabush@gmail.com>
2024-10-02 19:14:47 +0000

    Scope within block

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,7 +3,7 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    //  Block-scoped const, different from outer task
+    // eslint-disable-line no-unused-vars
 		const task = true;
     
 		const task2 = false;
