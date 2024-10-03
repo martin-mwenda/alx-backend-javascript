f49926e333387741c4e70e26a707f3a23b62d6d1
Martin Mwirigi <mwirigi.8kabush@gmail.com>
2024-10-03 02:29:57 +0000

    promise return

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,10 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => [resolve, reject]);
+  return new Promise((resolve, reject) => {
+    /*eslint-disable */
+    if (true) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
 }
