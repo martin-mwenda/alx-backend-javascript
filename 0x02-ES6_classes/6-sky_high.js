5e1eb24a914b85691f53a2a34867ad5e309ae63a
Martin Mwirigi <mwirigi.8kabush@gmail.com>
2024-10-04 02:06:10 +0000

    sky high

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -3,8 +3,7 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-	   // Assign to a private attribute
-    this._floors = floors;
+    this._floors = floors; // Assign to a private attribute
   }
 
   // floors getter
