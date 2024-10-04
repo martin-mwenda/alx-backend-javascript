8cf4ac69a35e777c8fb4d558de0c5f660b19018f
Martin Mwirigi <mwirigi.8kabush@gmail.com>
2024-10-04 01:35:03 +0000

    make some classrooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,15 +1,12 @@
-// Import the ClassRoom class from 0-classroom.js
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
-// Function initializes rooms
 function initializeRooms() {
   // Create and return an array of ClassRoom objects with sizes 19, 20, and 34
   return [
     new ClassRoom(19),
     new ClassRoom(20),
-    new ClassRoom(34)
+    new ClassRoom(34),
   ];
 }
 
 export default initializeRooms;
-
