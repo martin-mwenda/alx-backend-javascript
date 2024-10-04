c56c041e97c1d15bd0fac988abfc9c4103c002e3
Martin Mwirigi <mwirigi.8kabush@gmail.com>
2024-10-04 01:49:31 +0000

    class Currency

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,36 @@
+class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  // Getter and Setter for code
+  set code(code) {
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a String');
+    }
+    this._code = code;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  // Getter and Setter for name
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a String');
+    }
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
+
+export default Currency;
