3935256b0b962a8a4a6841851fbe5ffd7bd06569
Martin Mwirigi <mwirigi.8kabush@gmail.com>
2024-10-05 02:01:59 +0000

    ES6 method properties

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,18 @@
+import getBudgetObject from './7-getBudgetObject';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+	  // Method to return the income formatted in dollars
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
+	  // Method to return the income formatted in euros
+    getIncomeInEuros(income) {
+      return `${income} euros`;
+    },
+  };
+
+  return fullBudget;
+}
