ef9828f2ca5cc6bf98bc354d98ef9f1407e47a28
Martin Mwirigi <mwirigi.8kabush@gmail.com>
2024-10-09 02:47:34 +0000

    Set data structure

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,8 @@
+/**
+ *  function, which returns a Set from an array
+ *  takes an array of any type of element as its argument
+ */
+export default function setFromArray(array) {
+  const set = new Set(array);
+  return set;
+}
