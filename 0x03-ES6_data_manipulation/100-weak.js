6aab181d6eae949e1dcd032b02770074880e0c5a
Martin Mwirigi <mwirigi.8kabush@gmail.com>
2024-10-09 03:04:40 +0000

    Weak link data structure

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,3 +1,6 @@
+/**
+ * function queryAPI that accepts an endpoint argument, you can use the following code structure
+ */
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
@@ -7,6 +10,7 @@ export function queryAPI(endpoint) {
 
   weakMap.set(endpoint, called);
 
+  // Check if the endpoint is valid
   if (called >= 5) {
     throw Error('Endpoint load is high');
   }
